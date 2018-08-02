2c49a9bf7fdbc1ad00c9902ae031ba7eb6fe3443
roca <romulocintra@gmail.com>
2018-08-03 01:20:53 +0200

    refactor(o-alert):update css theme and add id instead of name

--- components/o-alert/src/components/o-alert/o-alert.tsx
+++ components/o-alert/src/components/o-alert/o-alert.tsx
@@ -8,21 +8,21 @@ import { Component , Prop} from '@stencil/core';
   shadow: true
 })
 export class OAlertComponent {
-  @Prop() name?: string;
+  @Prop() id?: string;
   @Prop() align: string = 'center' // left,right,center ;
   @Prop() type: string = 'default' //default, error, warning , info , success;
   @Prop() line: boolean= false;
 
 
   render() {
-    const types = ['info', 'success','error','warning','default'];
+    const types = ['info', 'success','error','default','warning'];
 
     const cssClassName = types.includes(this.type) && this.line ? `o-alert-line-${this.type}`
                                                                 : `o-alert-${this.type}`;
     const typeClasses : CssClassMap = {
       'o-alert' : true,
-      'o-alert-text-light' : types.slice(0,3).includes(this.type) && !this.line,
-      'o-alert-text-dark'  : types.slice(3,5).includes(this.type) || this.line,
+      'o-alert-text-light' : types.slice(0,4).includes(this.type) && !this.line,
+      'o-alert-text-dark'  : types.slice(4,5).includes(this.type) || this.line,
       ... {[cssClassName]:true }
     }
 
@@ -33,7 +33,7 @@ export class OAlertComponent {
     }
 
     return (
-      <div id={this.name} class={typeClasses} role="alert">
+      <div id={this.id} class={typeClasses} role="alert">
             <slot name="start"></slot>
             <span class={alignClasses}><slot></slot></span>
             <slot name="end"></slot>
