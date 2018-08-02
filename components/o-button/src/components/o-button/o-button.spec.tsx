5a24e40596cc9e39c9393ee5b889f2422d6446a5
roca <romulocintra@gmail.com>
2018-08-03 00:25:06 +0200

    test: update unit tests

--- components/o-button/src/components/o-button/o-button.spec.tsx
+++ components/o-button/src/components/o-button/o-button.spec.tsx
@@ -1,27 +1,26 @@
-import { OAlertComponent } from './o-button';
+import { OButtonComponent } from './o-button';
 import { TestWindow } from '@stencil/core/dist/testing';
 
 describe('o-button component', () => {
   it('should build', () => {
-    expect(new OAlertComponent()).toBeTruthy();
+    expect(new OButtonComponent()).toBeTruthy();
   });
 
   describe('rendering', () => {
-    let element: HTMLOAlertElement
+    let element: OButtonComponent
     let testWindow: TestWindow;
     beforeEach(async () => {
       testWindow = new TestWindow();
       element = await testWindow.load({
-        components: [OAlertComponent],
-        html: '<o-button>Hello, World! I\'m Peter</o-button>'
+        components: [OButtonComponent],
+        html: '<o-button>Summit</o-button>'
       });
     });
 
 
     it('check if text renders', async () => {
-      element.line = true;
       await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter');
+      expect(element.textContent.trim()).toEqual('Summit');
     });
 
   });
